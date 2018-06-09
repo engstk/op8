2caa66938215e82d4b9ca769ddeb947bcf3c2984
Wang Han <416810799@qq.com>
2018-06-08 22:36:57 -0700

    fs: sync: Avoid calling fdget without fdput
    When adding fsync support, we check for fsync_enabled() in several
    cases but it appears that we should fdput() after fdget() but the
    current code just check for fsync_enabled and directly return in
    some cases after calling fdget().
    
    Fix it by checking fsync_enabled first and move the f initialization
    after the check. Also remove some unnecessary fsync_enabled checks as
    this will be checked later in do_fsync().
    
    Signed-off-by: Wang Han <416810799@qq.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- fs/sync.c
+++ fs/sync.c
@@ -166,13 +166,14 @@ void emergency_sync(void)
  */
 SYSCALL_DEFINE1(syncfs, int, fd)
 {
-	struct fd f = fdget(fd);
+	struct fd f;
 	struct super_block *sb;
 	int ret;
 
 	if (!fsync_enabled)
 		return 0;
 
+	f = fdget(fd);
 	if (!f.file)
 		return -EBADF;
 	sb = f.file->f_path.dentry->d_sb;
@@ -221,9 +222,6 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
-	if (!fsync_enabled)
-		return 0;
-		
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
@@ -234,7 +232,7 @@ extern void ohm_schedstats_record(int sched_type, int fg, u64 delta_ms);
 
 static int do_fsync(unsigned int fd, int datasync)
 {
-	struct fd f = fdget(fd);
+	struct fd f;
 	int ret = -EBADF;
 	
 	if (!fsync_enabled)
@@ -244,6 +242,7 @@ static int do_fsync(unsigned int fd, int datasync)
 	unsigned long oneplus_fsync_time = jiffies;
 #endif
 
+	f = fdget(fd);
 	if (f.file) {
 		ret = vfs_fsync(f.file, datasync);
 		fdput(f);
@@ -258,17 +257,11 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-	if (!fsync_enabled)
-		return 0;
-
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-	if (!fsync_enabled)
-		return 0;
-		
 	return do_fsync(fd, 1);
 }
 

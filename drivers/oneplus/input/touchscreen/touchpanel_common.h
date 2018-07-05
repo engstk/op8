8b84904e07975f64d41c08ef5035453c64ee70b9
dianlujitao <dianlujitao@lineageos.org>
2018-07-06 00:06:54 +0200

    input: touchscreen: Expose every possible gesture
    Change-Id: I967893f8fe2797cf67d34aa81040be835d030e82
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/oneplus/input/touchscreen/touchpanel_common.h
+++ drivers/oneplus/input/touchscreen/touchpanel_common.h
@@ -59,6 +59,22 @@
 #define SingleTap           15  // single tap
 #define Sgestrue            14  // S
 
+#define KEY_GESTURE_W               246
+#define KEY_GESTURE_M               247
+#define KEY_GESTURE_S               248
+#define KEY_DOUBLE_TAP              KEY_WAKEUP
+#define KEY_GESTURE_CIRCLE          250
+#define KEY_GESTURE_TWO_SWIPE       251
+#define KEY_GESTURE_UP_ARROW        252
+#define KEY_GESTURE_LEFT_ARROW      253
+#define KEY_GESTURE_RIGHT_ARROW     254
+#define KEY_GESTURE_DOWN_ARROW      255
+#define KEY_GESTURE_SWIPE_LEFT      KEY_F5
+#define KEY_GESTURE_SWIPE_DOWN      KEY_F6
+#define KEY_GESTURE_SWIPE_RIGHT     KEY_F7
+#define KEY_GESTURE_SWIPE_UP        KEY_F8
+#define KEY_GESTURE_SINGLE_TAP      KEY_F9
+
 #define BIT0 (0x1 << 0)
 #define BIT1 (0x1 << 1)
 #define BIT2 (0x1 << 2)

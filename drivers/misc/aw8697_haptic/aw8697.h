29e3396ed0689e91b9643d4c7e2143c1172adb9b
flar2 <asegaert@gmail.com>
2019-05-24 11:16:54 -0400

    aw8697: add haptic level adjustment
    eng.stk: adapt for OP8 A12 source
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/misc/aw8697_haptic/aw8697.h
+++ drivers/misc/aw8697_haptic/aw8697.h
@@ -447,6 +447,7 @@ struct aw8697 {
     int index;
     int vmax;
     int gain;
+    int level;
     unsigned int gun_type;      //hch 20190917
     unsigned int bullet_nr; //hch 20190917
     unsigned int gun_mode;

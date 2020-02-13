d5195fef5c0e5b81ab82e961f956af65f81f1b58
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2020-02-13 11:38:47 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvements.
    
    Change-Id: I780f52ebc3cb69ee2c497d2d2f8e8fddd789421d
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5198,6 +5198,7 @@ unsigned int sched_lib_mask_force;
 bool is_sched_lib_based_app(pid_t pid)
 {
 	const char *name = NULL;
+	char *lib_list, *libname;
 	struct vm_area_struct *vma;
 	char path_buf[LIB_PATH_LENGTH];
 	bool found = false;
@@ -5207,11 +5208,14 @@ bool is_sched_lib_based_app(pid_t pid)
 	if (strnlen(sched_lib_name, LIB_PATH_LENGTH) == 0)
 		return false;
 
+	lib_list = kstrdup(sched_lib_name, GFP_KERNEL);
+
 	rcu_read_lock();
 
 	p = find_process_by_pid(pid);
 	if (!p) {
 		rcu_read_unlock();
+		kfree(lib_list);
 		return false;
 	}
 
@@ -5231,10 +5235,12 @@ bool is_sched_lib_based_app(pid_t pid)
 			if (IS_ERR(name))
 				goto release_sem;
 
-			if (strnstr(name, sched_lib_name,
+			while ((libname = strsep(&lib_list, ","))) {
+				if (strnstr(name, libname,
 					strnlen(name, LIB_PATH_LENGTH))) {
-				found = true;
-				break;
+					found = true;
+					break;
+				}
 			}
 		}
 	}
@@ -5244,6 +5250,7 @@ release_sem:
 	mmput(mm);
 put_task_struct:
 	put_task_struct(p);
+	kfree(lib_list);
 	return found;
 }
 

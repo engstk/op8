9f774b89a4d3ba0d27c6fe7296e982fbe1ee528f
engstk <eng.stk@sapo.pt>
2020-04-02 15:30:07 +0100

    fs: sync: set proper permissions for fsync_enabled
    fixes "[    0.359222] [    0.359218]@7 Attribute fsync_enabled: Invalid permissions 0755"
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- fs/sync.c
+++ fs/sync.c
@@ -23,7 +23,7 @@
 #endif
 
 bool fsync_enabled = true;
-module_param(fsync_enabled, bool, 0755);
+module_param(fsync_enabled, bool, 0644);
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)

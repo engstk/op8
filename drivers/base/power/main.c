68fc10236bceebd01579f2d8f1e79e977a8a33d6
Kristof Petho <kristof.petho@gmail.com>
2020-05-11 10:04:24 +0200

    Silence CPU state log in dmesg
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -133,7 +133,7 @@ void device_pm_add(struct device *dev)
 	device_pm_check_callbacks(dev);
 	mutex_lock(&dpm_list_mtx);
 	if (dev->parent && dev->parent->power.is_prepared)
-		dev_warn(dev, "parent %s should not be sleeping\n",
+		dev_dbg(dev, "parent %s should not be sleeping\n",
 			dev_name(dev->parent));
 	list_add_tail(&dev->power.entry, &dpm_list);
 	dev->power.in_dpm_list = true;

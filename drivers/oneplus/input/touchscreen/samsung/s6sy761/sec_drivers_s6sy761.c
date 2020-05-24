fd922595ba230c1b2e23697bf433fe2bc578d521
Kristof Petho <kristof.petho@gmail.com>
2020-05-24 10:29:31 +0200

    touchscreen: disable debugging
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/oneplus/input/touchscreen/samsung/s6sy761/sec_drivers_s6sy761.c
+++ drivers/oneplus/input/touchscreen/samsung/s6sy761/sec_drivers_s6sy761.c
@@ -31,17 +31,17 @@ struct pm_qos_request pm_qos_req_stp;
 
 /****************** Start of Log Tag Declear and level define*******************************/
 #define TPD_DEVICE "sec-s6sy761"
-#define TPD_INFO(a, arg...)  pr_err("[TP]"TPD_DEVICE ": " a, ##arg)
+#define TPD_INFO(a, arg...)  pr_debug("[TP]"TPD_DEVICE ": " a, ##arg)
 #define TPD_DEBUG(a, arg...)\
 	do{\
 		if (LEVEL_DEBUG == tp_debug)\
-		pr_err("[TP]"TPD_DEVICE ": " a, ##arg);\
+		pr_debug("[TP]"TPD_DEVICE ": " a, ##arg);\
 	}while(0)
 
 #define TPD_DETAIL(a, arg...)\
 	do{\
 		if (LEVEL_BASIC != tp_debug)\
-		pr_err("[TP]"TPD_DEVICE ": " a, ##arg);\
+		pr_debug("[TP]"TPD_DEVICE ": " a, ##arg);\
 	}while(0)
 
 #define TPD_DEBUG_NTAG(a, arg...)\

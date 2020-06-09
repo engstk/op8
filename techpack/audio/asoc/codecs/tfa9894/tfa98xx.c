a7b25165d520f8975d7b05d78715684780f8fa62
Kristof Petho <kristof.petho@gmail.com>
2020-06-09 11:01:41 +0200

    techpack: audio: fix coding
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- techpack/audio/asoc/codecs/tfa9894/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa9894/tfa98xx.c
@@ -1901,8 +1901,8 @@ static void tfa98xx_add_widgets(struct tfa98xx *tfa98xx)
 	unsigned int num_dapm_widgets = ARRAY_SIZE(tfa98xx_dapm_widgets_common);
 
 //add by Multimedia,do not add the following non-used widgets to hold mic.
-    if(1)
-        return;
+	if(1)
+		return;
 
 	widgets = devm_kzalloc(&tfa98xx->i2c->dev,
 			sizeof(struct snd_soc_dapm_widget) *
--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -1243,9 +1243,6 @@ static int msm_routing_get_adm_topology(int fedai_id, int session_type,
 	pr_debug("%s: fedai_id %d, session_type %d, be_id %d\n",
 	       __func__, fedai_id, session_type, be_id);
 
-	if (cal_data == NULL)
-		goto done;
-
 	app_type = fe_dai_app_type_cfg[fedai_id][session_type][be_id].app_type;
 	acdb_dev_id =
 		fe_dai_app_type_cfg[fedai_id][session_type][be_id].acdb_dev_id;
@@ -1268,7 +1265,6 @@ static int msm_routing_get_adm_topology(int fedai_id, int session_type,
 			pr_debug("%s: Force using topology %d\n", __func__, topology);
 		}
 	}
-done:
 	pr_debug("%s: Using topology %d\n", __func__, topology);
 	return topology;
 }

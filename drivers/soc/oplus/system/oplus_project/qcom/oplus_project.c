2307fdeeb691e3814326f0add79b1b5f6210477d
engstk <eng.stk@sapo.pt>
2022-07-01 17:54:22 +0100

    drivers: fix build and linking errors
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/soc/oplus/system/oplus_project/qcom/oplus_project.c
+++ drivers/soc/oplus/system/oplus_project/qcom/oplus_project.c
@@ -401,7 +401,7 @@ static void update_manifest(struct proc_dir_entry *parent)
 		"/vendor/odm/etc/vintf/manifest_dsds.xml",
 	};
 	mm_segment_t fs;
-	char *substr = strstr(boot_command_line, "simcardnum.doublesim=");
+	char *substr = strstr(saved_command_line, "simcardnum.doublesim=");
 
 	if (!substr)
 		return;
@@ -430,7 +430,7 @@ static void update_telephony_manifest(struct proc_dir_entry *parent)
 		"/vendor/odm/etc/vintf/telephony_manifest_dsds.xml",
 	};
 	mm_segment_t fs;
-	char *substr = strstr(boot_command_line, "simcardnum.doublesim=");
+	char *substr = strstr(saved_command_line, "simcardnum.doublesim=");
 
 	if (!substr)
 		return;

f40133dc392f521c2befe5b2da81d48ec8757490
engstk <eng.stk@sapo.pt>
2023-04-11 15:59:25 +0100

    Merge lineage-20.0 from LineageOS/android_kernel_oneplus_sm8250
    commit: 0ff2b740e1737ecf08a0b7292cf1c2707f7a76e2
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -3859,7 +3859,6 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	mutex_unlock(&fl->perf_mutex);
 	mutex_destroy(&fl->perf_mutex);
 	mutex_destroy(&fl->map_mutex);
-	mutex_destroy(&fl->pm_qos_mutex);
 	mutex_destroy(&fl->internal_map_mutex);
 	mutex_destroy(&fl->pm_qos_mutex);
 	kfree(fl);

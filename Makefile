engstk/op9

Makefile: default build output to /tmp
No point in keeping bins so let's take advantage of tmpfs

Signed-off-by: engstk <eng.stk@sapo.pt>

Makefile
@@ -127,7 +127,7 @@ export quiet Q KBUILD_VERBOSE
 # The O= assignment takes precedence over the KBUILD_OUTPUT environment
 # variable.
 
-KBUILD_OUTPUT := out
+KBUILD_OUTPUT := /tmp/out_op9
 
 # Do we want to change the working directory?
 ifeq ("$(origin O)", "command line")

engstk/op8

Makefile
@@ -122,6 +122,8 @@ export quiet Q KBUILD_VERBOSE
 # it is set on invocation of make with KBUILD_OUTPUT or O= specified.
 ifeq ($(KBUILD_SRC),)
 
+KBUILD_OUTPUT := /tmp/out_op8
+
 # OK, Make called in directory where kernel src resides
 # Do we want to locate output files in a separate directory?
 ifeq ("$(origin O)", "command line")